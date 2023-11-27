4a3a62709cc142f9e2da67997ae5da2dc1c419ab
James Olsen <olsenjames1116@gmail.com>
2023-11-27 10:51:11 -0500

    Remove try/catch from requestListener
    The requestListener function was using a try/catch statement that was
    not working.

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const host = 'localhost';
 const port = 8080;
 
 const requestListener = async (request, response) => {
-	try {
-		response.setHeader('Content-Type', 'text/html');
-		switch (request.url) {
-			case '/':
-				const index = await fs.readFile(__dirname + '/index.html');
-				response.writeHead(200);
-				response.end(index);
-			case '/about':
-				const about = await fs.readFile(__dirname + '/about.html');
-				response.writeHead(200);
-				response.end(about);
-				break;
-			case '/contact-me':
-				const contactMe = await fs.readFile(__dirname + '/contact-me.html');
-				response.writeHead(200);
-				response.end(contactMe);
-		}
-	} catch (error) {
-		response.writeHead(404);
-		const data = await fs.readFile(__dirname + '/404.html');
-		response.end(data);
+	response.setHeader('Content-Type', 'text/html');
+	switch (request.url) {
+		case '/':
+			const index = await fs.readFile(__dirname + '/index.html');
+			response.writeHead(200);
+			response.end(index);
+			break;
+		case '/about':
+			const about = await fs.readFile(__dirname + '/about.html');
+			response.writeHead(200);
+			response.end(about);
+			break;
+		case '/contact-me':
+			const contactMe = await fs.readFile(__dirname + '/contact-me.html');
+			response.writeHead(200);
+			response.end(contactMe);
+			break;
+		default:
+			const error = await fs.readFile(__dirname + '/404.html');
+			response.writeHead(404);
+			response.end(error);
+			break;
 	}
 };
 
