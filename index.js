620276f868657471dc26eafb20666763fd24d943
James Olsen <olsenjames1116@gmail.com>
2023-11-26 21:28:23 -0500

    Serve index.html using fs promises

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs/promises');
 
-http
-	.createServer((req, res) => {
-		fs.readFile('./index.html', (error, data) => {
-			res.writeHead(200, { 'Content-Type': 'text/html' });
-			res.end(data);
-		});
-	})
-	.listen(8080);
+const host = 'localhost';
+const port = 8080;
+
+const requestListener = async (request, response) => {
+	try {
+		const data = await fs.readFile(__dirname + '/index.html');
+		response.setHeader('Content-Type', 'text/html');
+		response.writeHead(200);
+		response.end(data);
+	} catch (error) {
+		response.writeHead(404, { 'Content-Type': 'text/html' });
+		fs.readFile(__dirname + '/404.html');
+		response.end();
+	}
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+	console.log(`Server is running on http://${host}:${port}`);
+});
