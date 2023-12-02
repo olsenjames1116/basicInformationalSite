65dffe3c38e2e9220d6374b4ac18199b86c1e568
James Olsen <olsenjames1116@gmail.com>
2023-12-02 10:28:02 -0500

    Serve error HTML file for 404

--- index.js
+++ index.js
@@ -11,4 +11,6 @@ app.get('/contact-me', (req, res) =>
 	res.sendFile(`${__dirname}/contact-me.html`)
 );
 
+app.use((req, res, next) => res.status(404).sendFile(`${__dirname}/404.html`));
+
 app.listen(port, () => console.log(`Now listening on port: ${port}`));
