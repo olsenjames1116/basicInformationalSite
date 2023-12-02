8530de1953d89b00a1b943210bf9ae9c8a4a3526
James Olsen <olsenjames1116@gmail.com>
2023-12-02 09:38:29 -0500

    Serve non-error HTML files

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
 
-const host = 'localhost';
 const port = 8080;
+
+app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
+
+app.get('/about', (req, res) => res.sendFile(`${__dirname}/about.html`));
+
+app.get('/contact-me', (req, res) =>
+	res.sendFile(`${__dirname}/contact-me.html`)
+);
 
 app.listen(port, () => console.log(`Now listening on port: ${port}`));
